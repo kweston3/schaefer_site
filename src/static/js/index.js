ac5d628c867d9f0f9f03f81c861c38f33d8cf3bb
kweston3 <kalebweston23@gmail.com>
2024-12-16 14:10:34 -0600

    add testimonials

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,15 +1,20 @@
-document.getElementById("searchForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent form submission
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("searchForm");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
 
-  const query = document.getElementById("searchInput").value.toLowerCase();
-  const vehicleSections = document.querySelectorAll(".grid > div");
+      const query = document.getElementById("searchInput").value.toLowerCase();
+      const vehicleSections = document.querySelectorAll(".grid > div");
 
-  vehicleSections.forEach((section) => {
-    const textContent = section.textContent.toLowerCase();
-    if (textContent.includes(query)) {
-      section.style.display = "block"; // Show matching vehicles
-    } else {
-      section.style.display = "none"; // Hide non-matching vehicles
-    }
-  });
+      vehicleSections.forEach((section) => {
+        const textContent = section.textContent.toLowerCase();
+        if (textContent.includes(query)) {
+          section.style.display = "block"; // Show matching vehicles
+        } else {
+          section.style.display = "none"; // Hide non-matching vehicles
+        }
+      });
+    });
+  }
 });
